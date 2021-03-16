1589eaac76feeb1c3133c961b2d02ef9edae6ac6
sinamna <sina3050@gmail.com>
2021-03-16 19:07:32 +0330

    added generate hash method

--- database/database.go
+++ database/database.go
@@ -1,5 +1,6 @@
 package database
 import (
+	"fmt"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"log"
 	"context"
@@ -7,10 +8,16 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"time"
+	"ChizShortener/graph/model"
+	"crypto/sha256"
 )
+
 
 type Database struct{
 	client *mongo.Client
+}
+func (db *Database)getChizCollection()*mongo.Collection{
+	return db.client.Database("ChizDatabase").Collection("links")
 }
 func ConnectDb() *Database{
 	client, err := mongo.NewClient(options.Client().ApplyURI("localhost:27017"))
@@ -27,4 +34,17 @@ func ConnectDb() *Database{
 	return &Database{
 		client: client,
 	}
+}
+
+func (db *Database) saveLink(newLink *model.NewLink) *model.Link{
+
+}
+func generateHash(ctx context.Context,longLink string,db *Database) (string,error){
+	hash := fmt.Sprintf("%x", sha256.Sum256([]byte(longLink)))[:6]
+	linkCollection := db.getChizCollection()
+	var savedLink bson.M
+	if err := linkCollection.FindOne(ctx,bson.M{"shortLink":hash}).Decode(&savedLink); err!=nil{
+		return generateHash(ctx,longLink+"1",db)
+	}
+	return hash,nil
 }
